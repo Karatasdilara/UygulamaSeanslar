96e9a12515bbd8abe8d7826e2f1f34d5f7e0d06e
Karatasdilara <karatassdilara@gmail.com>
2023-07-25 20:40:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/uygulamaseanslar/Uygulama.kt
@@ -0,0 +1,11 @@
+package com.example.uygulamaseanslar
+
+import java.util.Scanner
+
+fun main() {
+    var scanner = Scanner(System.`in`)
+    println("İsminizi giriniz:")
+    var isim = scanner.next()
+    println("Yaşınızı giriniz:")
+    var yas = scanner.nextInt()
+}
